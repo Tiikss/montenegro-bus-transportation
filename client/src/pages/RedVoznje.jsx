ebef1e8db33eaff5a36caff3728f253ca2cb41e1
Clipz1204 <aleksa.sljivancanin.as@gmail.com>
2024-07-20 18:06:59 +0200

    Add RedVoznje page and its style

---
+++ client/src/pages/RedVoznje.jsx
@@ -0,0 +1,11 @@
+import "../styles/redvoznje.css";
+
+const RedVoznje = () => {
+  return (
+    <div className="red-voznje-content">
+      <h1>Red vožnje</h1>
+    </div>
+  );
+};
+
+export default RedVoznje;
