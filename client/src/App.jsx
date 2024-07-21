a7c39c8aabe219166e3aea3ca1b69eea144b17b5
Jovan02 <jovannikolic2801@gmail.com>
2024-07-21 16:55:44 +0200

    Add DodajLiniju route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,46 +5,51 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import RedVoznje from "./pages/RedVoznje";
 import PrevoznikPanel from "./pages/PrevoznikPanel";
+import DodajLiniju from "./pages/DodajLiniju";
 
 const Layout = () => {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
+    );
 };
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
+    {
         path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/red-voznje",
-        element: <RedVoznje />,
-      },
-      {
-        path: "/prevoznik-panel",
-        element: <PrevoznikPanel />,
-      },
-    ],
-  },
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/red-voznje",
+                element: <RedVoznje />,
+            },
+            {
+                path: "/prevoznik-panel",
+                element: <PrevoznikPanel />,
+            },
+            {
+                path: "/dodaj-liniju",
+                element: <DodajLiniju />,
+            },
+        ],
+    },
 ]);
 
 function App() {
-  return (
-    <div className="app">
-      <div className="container">
-        <RouterProvider router={router} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="container">
+                <RouterProvider router={router} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
