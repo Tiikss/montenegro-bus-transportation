c729018577def28f67d21f14da926adb6884e901
Jovan02 <jovannikolic2801@gmail.com>
2024-07-22 18:24:13 +0200

    Fix isEdit on RedVoznje

--- client/src/components/TableRedVoznjeContent.jsx
+++ client/src/components/TableRedVoznjeContent.jsx
@@ -1,29 +1,29 @@
 import "../styles/tableredvoznjecontent.css";
 import Linija from "./Linija";
 
-const TableRedVoznjeContent = ({ departure }) => {
-  console.log(departure.stations);
-  return (
-    <li className="col-content">
-      <div className="col table-row-description">
-        <h3>Broj autobusa: {departure.busID}</h3>
-        <h4>
-          {departure.startingPoint}-{departure.destination}
-          <br></br>
-          <br></br>
-          {departure.departureTime}-{departure.arrivalTime}
-        </h4>
-        <div className="table-line-container">
-          <Linija
-            stations={departure.stations}
-            isEdit={true}
-            handleEditClick={() => {}}
-            handleDeleteClick={() => {}}
-          />
-        </div>
-      </div>
-    </li>
-  );
+const TableRedVoznjeContent = ({ departure, isEdit }) => {
+    console.log(departure.stations);
+    return (
+        <li className="col-content">
+            <div className="col table-row-description">
+                <h3>Broj autobusa: {departure.busID}</h3>
+                <h4>
+                    {departure.startingPoint}-{departure.destination}
+                    <br></br>
+                    <br></br>
+                    {departure.departureTime}-{departure.arrivalTime}
+                </h4>
+                <div className="table-line-container">
+                    <Linija
+                        stations={departure.stations}
+                        isEdit={isEdit}
+                        handleEditClick={() => {}}
+                        handleDeleteClick={() => {}}
+                    />
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default TableRedVoznjeContent;
