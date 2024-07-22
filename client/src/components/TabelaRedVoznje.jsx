55194d92cd2da6609a8f38882d56b9a9df3c8111
Clipz1204 <aleksa.sljivancanin.as@gmail.com>
2024-07-22 13:28:22 +0200

    Transfer leftover code from table to row component

--- client/src/components/TabelaRedVoznje.jsx
+++ client/src/components/TabelaRedVoznje.jsx
@@ -6,28 +6,12 @@ import TableRedVoznjeRow from "./TableRedVoznjeRow";
 import TableRedVoznjeContent from "./TableRedVoznjeContent";
 
 const TabelaRedVoznje = ({ isEdit }) => {
-    const handleClick = (e) => {
-        const content = e.target.parentElement.parentElement.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.padding = "0 24px";
-            content.style.maxHeight = null;
-            content.style.marginBottom = "0";
-            content.style.border = "none";
-        } else {
-            content.style.border = "1px solid #e0e0e0";
-            content.style.marginBottom = "25px";
-            content.style.padding = "40px 24px";
-            content.style.maxHeight =
-                content.childNodes[0].childNodes[0].scrollHeight + 40 + "px";
-        }
-    };
-
     return (
         <ul className="red-voznje-table">
             <TabelaRedVoznjeHeader isEdit={isEdit} />
-            <TableRedVoznjeRow isEdit={isEdit} handleClick={handleClick} />
+            <TableRedVoznjeRow isEdit={isEdit}/>
             <TableRedVoznjeContent />
-            <TableRedVoznjeRow isEdit={isEdit} handleClick={handleClick} />
+            <TableRedVoznjeRow isEdit={isEdit}/>
             <TableRedVoznjeContent />
         </ul>
     );
--- client/src/components/TableRedVoznjeRow.jsx
+++ client/src/components/TableRedVoznjeRow.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const TableRedVoznjeRow = ({isEdit, handleClick}) => {
+const TableRedVoznjeRow = ({isEdit}) => {
+    const handleClick = (e) => {
+        const content = e.target.parentElement.parentElement.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.padding = "0 24px";
+            content.style.maxHeight = null;
+            content.style.marginBottom = "0";
+            content.style.border = "none";
+        } else {
+            content.style.border = "1px solid #e0e0e0";
+            content.style.marginBottom = "25px";
+            content.style.padding = "40px 24px";
+            content.style.maxHeight =
+                content.childNodes[0].childNodes[0].scrollHeight + 40 + "px";
+        }
+    };
+
     return(
         <li className="red-voznje-table-row red-voznje-row-hover">
                 {isEdit && <div className="col col-0">Podgorica</div>}
