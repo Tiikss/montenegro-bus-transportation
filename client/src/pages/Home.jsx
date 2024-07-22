e4fa48b1915ed3d2391719d79c4994fc4d65d494
Tiikss <trakocevic7@gmail.com>
2024-07-22 14:56:10 +0200

    comm

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/home.css"
+import slika1 from "../images/group-buses-driving-along-road-sunset.jpg"
 
 const Home = () => {
     return (
         <main className="home-body">
-            <h1>Home</h1>
+            <img src={slika1} alt="slika1" className="home-img" />
         </main>
     );
 }
