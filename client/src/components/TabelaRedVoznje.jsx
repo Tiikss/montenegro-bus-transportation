8152d2a69944f0fdfa7f87afbda0a827e9cdfe69
Clipz1204 <aleksa.sljivancanin.as@gmail.com>
2024-07-22 13:43:33 +0200

    Add dummy data to test mapping

--- client/src/components/TabelaRedVoznje.jsx
+++ client/src/components/TabelaRedVoznje.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/tabela-red-voznje.css";
 import TabelaRedVoznjeHeader from "./TableRedVoznjeHeader";
@@ -6,13 +7,37 @@ import TableRedVoznjeRow from "./TableRedVoznjeRow";
 import TableRedVoznjeContent from "./TableRedVoznjeContent";
 
 const TabelaRedVoznje = ({ isEdit }) => {
+    const [departures, setDepartures] = useState([{
+        startingPoint: "Podgorica",
+        destination: "Beograd",
+        busID: "1",
+        departureTime: "12:00",
+        travelTime: "2h",
+        arrivalTime: "14:00",
+        carrier: "Lasta",
+        price: "10€",
+    },
+    {
+        startingPoint: "Podgorica",
+        destination: "Novi sad",
+        busID: "1",
+        departureTime: "12:00",
+        travelTime: "2h",
+        arrivalTime: "14:00",
+        carrier: "Lasta",
+        price: "10€",
+    }
+    ]);
+
     return (
         <ul className="red-voznje-table">
             <TabelaRedVoznjeHeader isEdit={isEdit} />
-            <TableRedVoznjeRow isEdit={isEdit}/>
-            <TableRedVoznjeContent />
-            <TableRedVoznjeRow isEdit={isEdit}/>
-            <TableRedVoznjeContent />
+            {departures ? departures.map((departure) => (
+                <>
+                    <TableRedVoznjeRow departure={departure} isEdit={isEdit}/>
+                    <TableRedVoznjeContent departure={departure}/>
+                </>
+            )): null}
         </ul>
     );
 };
