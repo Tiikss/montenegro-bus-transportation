590d287cf0e430225965d4de3b3a2c061238f19e
Tiikss <trakocevic7@gmail.com>
2024-07-23 02:07:04 +0200

    Mapa i inputi

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,53 @@
 import React from "react";
-import "../styles/home.css"
-import slika1 from "../images/group-buses-driving-along-road-sunset.jpg"
+import "../styles/home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const defaultProps = {
+    center: {
+        lat: 42.5,
+        lng: 19.3
+    },
+    zoom: 8
+};
 
 const Home = () => {
     return (
         <main className="home-body">
-            <img src={slika1} alt="slika1" className="home-img" />
+            <div className="welcome-div">
+                <h1 className="welcome-ttl">Dobrodošli!</h1>
+            </div>
+
+            <div className="red-div">
+                <h1>Red vožnje</h1>
+
+                <div className="choose">
+                    <label htmlFor="beginning">Od:</label>
+                    <input type="text" placeholder="Unesite polaznu stanicu" id="beginning"/>
+                    <label htmlFor="end">Do:</label>
+                    <input type="text" placeholder="Unesite krajnju stanicu" id="end" />
+                    <label htmlFor="date">Datum:</label>
+                    <input type="date" id="date" />
+                    <button><FontAwesomeIcon icon={faSearch} style={{ marginRight: "5px" }} /> Pretraži</button>
+                </div>
+            </div>
+
+            <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "AIzaSyByhANtjCtr6kXzO0gSJKVW1oR2qzR46Nw" }}
+                    defaultCenter={defaultProps.center}
+                    defaultZoom={defaultProps.zoom}
+                >
+                    <AnyReactComponent
+                        lat={42.5}
+                        lng={19.3}
+                        text="Crna Gora"
+                    />
+                </GoogleMapReact>
+            </div>
         </main>
     );
 }
