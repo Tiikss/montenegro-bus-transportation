0b154a5a15307ab49fd0c5c51546866e490a4a60
Clipz1204 <aleksa.sljivancanin.as@gmail.com>
2024-07-25 14:14:59 +0200

    Add ticket reservation modal

---
+++ client/src/components/TicketReservation.jsx
@@ -0,0 +1,10 @@
+const TicketReservation = () => {
+    return (
+        <>
+            <div className="hidden" id="ticket-reservation-modal"></div>
+            <div className="hidden" id="overlay"></div>
+        </>
+    );
+};
+
+export default TicketReservation;
