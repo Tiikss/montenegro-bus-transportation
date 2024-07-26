c2084ab0a88bb174873e4d0c578fa20d6cde772e
Jovan02 <jovannikolic2801@gmail.com>
2024-07-26 14:35:10 +0200

    Fix ModalWindow

--- client/src/components/ModalWindow.jsx
+++ client/src/components/ModalWindow.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "../styles/modal-window.css";
 
-const ModalWindow = ({ isConfirmation, isOpen }) => {
+const ModalWindow = ({
+    isConfirmation,
+    isOpen,
+    setIsOpen,
+    title,
+    message,
+    setResponse,
+}) => {
+    const handleExitClick = (e) => {
+        setIsOpen(false);
+    };
+
     return (
         <>
+            <div className={"modal-container" + (isOpen ? "" : " hidden")}>
+                <h2>{title}</h2>
+                <p>{message}</p>
+
+                {isConfirmation && (
+                    <div className="modal-buttons">
+                        <button
+                            onClick={() => {
+                                setResponse(true);
+                                setIsOpen(false);
+                            }}
+                            className="modal-button"
+                        >
+                            Da
+                        </button>
+                        <button
+                            onClick={() => {
+                                setResponse(false);
+                                setIsOpen(false);
+                            }}
+                            className="modal-button"
+                        >
+                            Ne
+                        </button>
+                    </div>
+                )}
+            </div>
             <div
-                className={"modal-container" + (isOpen ? "" : " hidden")}
+                className={"overlay-modal" + (isOpen ? "" : " hidden")}
+                onClick={handleExitClick}
             ></div>
-            <div className={"overlay" + (isOpen ? "" : " hidden")}></div>
         </>
     );
 };
