082c8a8cbef579433f41f07885e1e09c86e4b239
Jovan02 <jovannikolic2801@gmail.com>
2024-07-27 11:30:16 +0200

    Create TabelaKorisnici

---
+++ client/src/components/TabelaKorisnici.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TabelaKorisnici = () => {
+    return (
+        <table className="tabela-korisnici">
+            <tr>
+                <th>ID</th>
+                <th>Ime</th>
+                <th>Prezime</th>
+                <th>Email</th>
+                <th>Broj telefona</th>
+                <th>Uloga</th>
+                <th>Ime kompanije</th>
+            </tr>
+        </table>
+    );
+};
+
+export default TabelaKorisnici;
