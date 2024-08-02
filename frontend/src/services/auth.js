5df6d8df04ab225cef83427b676caef111d60182
Jovan02 <jovannikolic2801@gmail.com>
2024-08-02 13:43:11 +0200

    Add register functionality

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -3,19 +3,24 @@ import axios from "axios";
 const URL = process.env.REACT_APP_API_URL;
 
 export const login = async (username, password) => {
-    const response = await axios.post(`${URL}/token`, { username, password }, {
-        headers: {
-            "ngrok-skip-browser-warning": "true"
+    const response = await axios.post(
+        `${URL}/token`,
+        { username, password },
+        {
+            headers: {
+                "ngrok-skip-browser-warning": "true",
+            },
         }
-    });
+    );
     return response.data;
-}
+};
 
-export const register = async (username, email, password) => {
-    const response = await axios.post(`${URL}/register`, { username, email, password }, {
+export const register = async (user) => {
+    console.log(user);
+    const response = await axios.post(`${URL}/signup`, user, {
         headers: {
-            "ngrok-skip-browser-warning": "true"
-        }
+            "ngrok-skip-browser-warning": "true",
+        },
     });
     return response.data;
-}
+};
