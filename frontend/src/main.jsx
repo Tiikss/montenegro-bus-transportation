d167f30a2ead49cca086b5dfdbca8a6bb5f1ad66
Jovan02 <jovannikolic2801@gmail.com>
2024-08-02 15:14:05 +0200

    Add interceptor for token

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { setupInterceptors } from "./Interceptor.js";
 
+setupInterceptors();
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <AuthContextProvider>
