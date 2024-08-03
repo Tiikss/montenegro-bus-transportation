ddaa28814b51a42d656f4dc69ab3ad93b9c0bb4b
Aleksa Šljivančanin <100388597+Clipz1204@users.noreply.github.com>
2024-08-03 22:23:52 +0200

    Dropdown menu updates (#3)
    * Add dropdown menu to home page
    
    * Fix style to pop up more
    
    * Add dropdown to timetable for cities
    
    * Add dropdown menus to TableStation for cities and countries

--- frontend/src/components/TableStation/TableStations.jsx
+++ frontend/src/components/TableStation/TableStations.jsx
@@ -1,14 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./table-stations.css";
 import { MapWrapper } from "../MapWrapper/MapWrapper";
+import { DropDownCard } from "../DropdownCard/DropdownCard";
 
 export const TableStations = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [allCities, setAllCities] = useState([
+        "Podgorica",
+        "Niksic",
+        "Bar",
+        "Budva",
+        "Kotor",
+    ]);
+    const [allCountries, setAllCountries] = useState([
+        "Crna Gora",
+        "Srbija",
+        "Hrvatska",
+        "Bosna i Hercegovina",
+        "Slovenija",
+    ]);
+    const [inputs, setInputs] = useState({
+        city: "",
+        country: "",
+    });
 
     const handleAddNewStation = (e) => {
         e.preventDefault();
         setIsModalOpen(true);
     };
+
+    const handleChange = (e) => {
+        setInputs((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    };
+
+    const handleSetSearch = (e) => {
+        setInputs((prev) => ({
+            ...prev,
+            [e.target.parentElement.parentElement.childNodes[0].id]:
+                e.target.innerText,
+        }));
+        if (e.target.parentElement.parentElement.childNodes[0].id === "city") {
+            setAllCities([]);
+        } else {
+            setAllCountries([]);
+        }
+    };
+
+    useEffect(() => {
+        const filteredCities = allCities.filter((item) =>
+            item.toLowerCase().includes(inputs.city.toLowerCase())
+        );
+        setAllCities(filteredCities);
+        if (inputs.city === "") {
+            setAllCities(["Podgorica", "Niksic", "Bar", "Budva", "Kotor"]);
+        }
+    }, [inputs.city]);
+
+    useEffect(() => {
+        const filteredCountries = allCountries.filter((item) =>
+            item.toLowerCase().includes(inputs.country.toLowerCase())
+        );
+        setAllCountries(filteredCountries);
+        if (inputs.country === "") {
+            setAllCountries([
+                "Crna Gora",
+                "Srbija",
+                "Hrvatska",
+                "Bosna i Hercegovina",
+                "Slovenija",
+            ]);
+        }
+    }, [inputs.country]);
 
     return (
         <>
@@ -56,9 +118,45 @@ export const TableStations = () => {
                 <label htmlFor="station-name">Ime:</label>
                 <input type="text" id="station-name" />
                 <label htmlFor="station-country">Drzava:</label>
-                <input type="text" id="station-country" />
+                <div className="add-line-input-container">
+                    <input
+                        type="text"
+                        id="country"
+                        value={inputs.country}
+                        onChange={handleChange}
+                    />
+                    <div className="dropdown-container">
+                        {inputs.country !== ""
+                            ? allCountries.map((item) => (
+                                  <DropDownCard
+                                      item={item}
+                                      key={item}
+                                      onClick={handleSetSearch}
+                                  />
+                              ))
+                            : null}
+                    </div>
+                </div>
                 <label htmlFor="station-city">Grad:</label>
-                <input type="text" id="station-city" />
+                <div className="add-line-input-container">
+                    <input
+                        type="text"
+                        id="city"
+                        value={inputs.city}
+                        onChange={handleChange}
+                    />
+                    <div className="dropdown-container">
+                        {inputs.city !== ""
+                            ? allCities.map((item) => (
+                                  <DropDownCard
+                                      item={item}
+                                      key={item}
+                                      onClick={handleSetSearch}
+                                  />
+                              ))
+                            : null}
+                    </div>
+                </div>
                 <MapWrapper stations={[]} isAdmin={true} />
                 {/* Odje treba opcija da se sa mape bira lokacija i da se stavi */}
                 <button className="adminpanel-button">Dodaj stanicu</button>
