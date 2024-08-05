15b7d0ca91e04b634f8d7bd2dca0f8037b6fc31e
Jovan02 <jovannikolic2801@gmail.com>
2024-08-05 19:54:28 +0200

    Add functionality to add company

--- frontend/src/components/TableCompanies/TableCompanies.jsx
+++ frontend/src/components/TableCompanies/TableCompanies.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { getCompanies } from "../../services/companies";
+import { getCompanies, addCompany } from "../../services/companies";
 import { PaginationNumbers } from "../PaginationNumbers/PaginationNumbers";
 import "./table-companies.css";
 
@@ -8,6 +8,9 @@ export const TableCompanies = () => {
     const [companies, setCompanies] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [numberOfPages, setNumberOfPages] = useState(1);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const [companyName, setCompanyName] = useState("");
 
     const fetchCompanies = async () => {
         try {
@@ -18,35 +21,82 @@ export const TableCompanies = () => {
         }
     };
 
+    const handleAddNewCompany = (e) => {
+        e.preventDefault();
+        try {
+            addCompany({ company_name: companyName });
+            fetchCompanies();
+            setIsModalOpen(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleChange = (e) => {
+        setCompanyName(e.target.value);
+    };
+
     useEffect(() => {
         fetchCompanies();
     }, []);
 
     return (
-        <div className="tabela-kompanije-content">
-            <table className="tabela-kompanije">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Naziv kompanije</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {companies.map((company) => (
-                        <tr key={company.id}>
-                            <td>{company.id}</td>
-                            <td>{company.company_name}</td>
+        <>
+            <button
+                className="adminpanel-button"
+                onClick={(e) => setIsModalOpen(true)}
+            >
+                Dodaj kompaniju
+            </button>
+
+            <div className="tabela-kompanije-content">
+                <table className="tabela-kompanije">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Naziv kompanije</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div className="pagination-numbers-container">
-                <PaginationNumbers
-                    selectedPageId={currentPage}
-                    setPageId={setCurrentPage}
-                    numberOfPages={numberOfPages}
-                />
+                    </thead>
+                    <tbody>
+                        {companies.map((company) => (
+                            <tr key={company.id}>
+                                <td>{company.id}</td>
+                                <td>{company.company_name}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className="pagination-numbers-container">
+                    <PaginationNumbers
+                        selectedPageId={currentPage}
+                        setPageId={setCurrentPage}
+                        numberOfPages={numberOfPages}
+                    />
+                </div>
             </div>
-        </div>
+            <div
+                className={"modal-container" + (isModalOpen ? "" : " hidden")}
+                style={{ display: isModalOpen ? "block" : "none" }}
+            >
+                <h2>Dodavanje nove kompanije</h2>
+                <form className="add-company-form">
+                    <label htmlFor="company-name">Naziv kompanije:</label>
+                    <input
+                        type="text"
+                        id="company-name"
+                        name="company-name"
+                        value={companyName}
+                        onChange={handleChange}
+                    />
+                    <button type="submit" onClick={handleAddNewCompany}>
+                        Dodaj
+                    </button>
+                </form>
+            </div>
+            <div
+                className={"overlay-modal" + (isModalOpen ? "" : " hidden")}
+                onClick={() => setIsModalOpen(false)}
+            ></div>
+        </>
     );
 };
