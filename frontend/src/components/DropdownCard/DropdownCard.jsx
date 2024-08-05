831d9a49200eb3ebe4f5829a7bcac70faec9481e
Jovan02 <jovannikolic2801@gmail.com>
2024-08-05 23:56:39 +0200

    Add option to DropDownCard to be used for companies

--- frontend/src/components/DropdownCard/DropdownCard.jsx
+++ frontend/src/components/DropdownCard/DropdownCard.jsx
@@ -3,7 +3,7 @@ import "./dropdowncard.css";
 export const DropDownCard = ({ item, key, onClick }) => {
     return (
         <div onClick={onClick} className="dropdown-card" key={key}>
-            <p className="ddcp">{item.address}</p>
+            <p className="ddcp">{item.address || item.company_name}</p>
         </div>
     );
 };
