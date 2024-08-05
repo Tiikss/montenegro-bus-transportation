bf374b1a585d6704e1ee15d9a6a33575d05223a7
Clipz1204 <aleksa.sljivancanin.as@gmail.com>
2024-08-05 16:19:07 +0200

    Link TimetableFiltered to api and make validation

--- frontend/src/pages/TimeTableFiltered/TimetableFiltered.jsx
+++ frontend/src/pages/TimeTableFiltered/TimetableFiltered.jsx
@@ -1,14 +1,37 @@
+import { useState, useEffect } from "react";
+import { getLinesFiltered } from "../../services/lines";
+
 export const TimetableFiltered = () => {
-    const path = window.location.pathname;
-    const pathSplit = path.split("/");
-    const start = pathSplit[2];
-    const end = pathSplit[3];
-    const date = pathSplit[4];
+    const [lines, setLines] = useState(null);
+
+    const fetchLines = async () => {
+        try {
+            const path = window.location.pathname;
+            const pathSplit = path.split("/");
+            const linesApi = await getLinesFiltered(
+                true,
+                1,
+                pathSplit[2],
+                pathSplit[3],
+                pathSplit[4]
+            );
+            setLines(linesApi);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchLines();
+    }, []);
+
     return (
-        <div>
-            <p>{start}</p>
-            <p>{end}</p>
-            <p>{date}</p>
-        </div>
+        <>
+            {lines && lines.length > 0 ? (
+                <div>Radi</div>
+            ) : (
+                <div>There are no lines available</div>
+            )}
+        </>
     );
 };
