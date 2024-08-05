f98b622a9fee3e32d10ccee7a0a918cb1aa94e42
Jovan02 <jovannikolic2801@gmail.com>
2024-08-05 23:57:00 +0200

    Add filter to getCompanies

--- frontend/src/services/companies.js
+++ frontend/src/services/companies.js
@@ -2,8 +2,16 @@ import axios from "axios";
 
 const URL = process.env.REACT_APP_API_URL;
 
-export const getCompanies = async () => {
-    const response = await axios.get(`${URL}/companies`, {
+export const getCompanies = async (filter) => {
+    if (!filter) {
+        const response = await axios.get(`${URL}/companies`, {
+            headers: {
+                "ngrok-skip-browser-warning": "true",
+            },
+        });
+        return response.data;
+    }
+    const response = await axios.get(`${URL}/companies?search=${filter}`, {
         headers: {
             "ngrok-skip-browser-warning": "true",
         },
