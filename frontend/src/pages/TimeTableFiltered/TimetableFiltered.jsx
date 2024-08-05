6e36fdfc11752bc33b0ad18a2fbfb2956c19b240
Clipz1204 <aleksa.sljivancanin.as@gmail.com>
2024-08-05 18:27:41 +0200

    Add loading for filtered timetable

--- frontend/src/pages/TimeTableFiltered/TimetableFiltered.jsx
+++ frontend/src/pages/TimeTableFiltered/TimetableFiltered.jsx
@@ -1,13 +1,21 @@
 import { useState, useEffect } from "react";
 import { getLinesFiltered } from "../../services/lines";
+import { TableTimetable } from "../../components/TableTimetable/TableTimetable";
+import "./timetablefiltered.css";
 
 export const TimetableFiltered = () => {
     const [lines, setLines] = useState(null);
+    const [lineDetails, setLineDetails] = useState(null);
 
     const fetchLines = async () => {
         try {
             const path = window.location.pathname;
             const pathSplit = path.split("/");
+            setLineDetails({
+                start: pathSplit[2],
+                end: pathSplit[3],
+                date: pathSplit[4],
+            });
             const linesApi = await getLinesFiltered(
                 true,
                 1,
@@ -28,9 +36,23 @@ export const TimetableFiltered = () => {
     return (
         <>
             {lines && lines.length > 0 ? (
-                <div>Radi</div>
+                <div className="red-voznje-content">
+                    <h1>
+                        Red vožnje za liniju: {lineDetails.start}-
+                        {lineDetails.end}
+                        <br />
+                        Dana: {lineDetails.date}
+                    </h1>
+                    <TableTimetable
+                        isEdit={false}
+                        isActive={1}
+                        filter={lineDetails}
+                    />
+                </div>
             ) : (
-                <div>There are no lines available</div>
+                <div className="filtered-timetable-container">
+                    <h1>Tražena linija ne postoji ili ne saobraća tog dana.</h1>
+                </div>
             )}
         </>
     );
