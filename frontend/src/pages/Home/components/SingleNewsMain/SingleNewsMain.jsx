d354a97bfc9c77999e285e910eefebf527cadb08
Tiikss <trakocevic7@gmail.com>
2024-08-06 15:36:10 +0200

    Formatiranje datuma na home-news kartici

--- frontend/src/pages/Home/components/SingleNewsMain/SingleNewsMain.jsx
+++ frontend/src/pages/Home/components/SingleNewsMain/SingleNewsMain.jsx
@@ -2,12 +2,14 @@ import "./singlenewsmain.css";
 import logo from "../../../../images/logo-crop.png";
 
 export const SingleNewsMain = ({ news, handleOpenModal }) => {
+    const date = news.created_date.split("T")[0].split("-");
+    const currDate = date[2] + "." + date[1] + "." + date[0];
     return (
         <div className="novost">
             <img src={logo} alt="logo" />
             <div className="ttl-and-date">
                 <h1>{news.title}</h1>
-                <p className="ndate">Objavljeno: {news.date}</p>
+                <p className="ndate">Objavljeno: {currDate}</p>
             </div>
             <p>{news.content.substring(0, 100)}...</p>
             <button onClick={() => handleOpenModal(news.title, news.content)}>
