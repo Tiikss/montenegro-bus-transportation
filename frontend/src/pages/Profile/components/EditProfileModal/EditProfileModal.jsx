0a27830a948f5ffa5f854c165eed44ca5e420a8f
Clipz1204 <aleksa.sljivancanin.as@gmail.com>
2024-08-06 18:21:19 +0200

    Add functionality to modal

--- frontend/src/pages/Profile/components/EditProfileModal/EditProfileModal.jsx
+++ frontend/src/pages/Profile/components/EditProfileModal/EditProfileModal.jsx
@@ -1,4 +1,44 @@
-export const EditProfileModal = ({ show, handleClose }) => {
+import { useState } from "react";
+import { updateUser } from "../../../../services/users";
+
+export const EditProfileModal = ({ show, handleClose, user }) => {
+    const [input, setInput] = useState(
+        user.phone_number == "000000000" ? null : user.phone_number
+    );
+    const [error, setError] = useState(null);
+
+    const handleInputChange = (event) => {
+        setInput(event.target.value);
+    };
+
+    const updateUserFunc = async () => {
+        try {
+            await updateUser(user.id, user);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        if (input === null || input === "") {
+            setError("Unesite broj telefona.");
+            return;
+        }
+
+        if (input.length !== 9) {
+            setError("Broj telefona mora sadržavati 9 cifara.");
+            return;
+        }
+
+        setError(null);
+
+        user.phone_number = input;
+
+        updateUserFunc();
+        window.location.reload();
+    };
+
     if (!show) {
         return null;
     }
@@ -12,7 +52,20 @@ export const EditProfileModal = ({ show, handleClose }) => {
                         ×
                     </button>
                 </div>
-                <input type="text" placeholder="Unesite broj telefona:" />
+                <input
+                    type="number"
+                    placeholder="Unesite broj telefona:"
+                    value={input}
+                    onChange={handleInputChange}
+                />
+                {error && <p className="error-message">{error}</p>}
+                <button
+                    className="btnsty"
+                    id="save-btn"
+                    onClick={(e) => handleClick(e)}
+                >
+                    Sačuvaj izmjene
+                </button>
             </div>
         </div>
     );
