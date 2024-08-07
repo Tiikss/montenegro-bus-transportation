bfeefaebf03b07bfa7b82cc89dff460429d24334
Jovan02 <jovannikolic2801@gmail.com>
2024-08-07 17:27:18 +0200

    Change column name

--- frontend/src/components/TableTimetable/components/TableTimetableHeader/TableTimetableHeader.jsx
+++ frontend/src/components/TableTimetable/components/TableTimetableHeader/TableTimetableHeader.jsx
@@ -8,9 +8,9 @@ export const TableTimetableHeader = ({ isEdit, isCarrier, isAdmin }) => {
         <li className="red-voznje-table-header">
             {isEdit && <div className="col col-0">Pocetna</div>}
             <div className="col col-1">Destinacija</div>
-            <div className="col col-3">Vrijeme polaska</div>
+            <div className="col col-3">Vrijeme Polaska</div>
             <div className="col col-4">Duzina puta</div>
-            <div className="col col-5">Vrijeme Polaska</div>
+            <div className="col col-5">Vrijeme Dolaska</div>
             {(!isEdit || isAdmin) && <div className="col col-6">Prevoznik</div>}
             <div className="col col-7">Cijena</div>
             <div className="col col-8"></div>
