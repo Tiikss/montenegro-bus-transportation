beac446a8ffd6b1020868adf87e9f1f470c9f682
Clipz1204 <aleksa.sljivancanin.as@gmail.com>
2024-08-07 14:51:52 +0200

    Connect modal to route

--- frontend/src/components/TicketReservation/TicketReservation.jsx
+++ frontend/src/components/TicketReservation/TicketReservation.jsx
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
+import { postTicket} from "../../services/ticket";
 import "./ticket-reservation.css";
 
-export const TicketReservation = () => {
+export const TicketReservation = ( {filterDate, price, route_id, company_id} ) => {
     const { user } = useContext(AuthContext);
 
     const handleModalClose = () => {
@@ -12,6 +13,53 @@ export const TicketReservation = () => {
         document.getElementById("overlay").classList.add("hidden");
         document.body.style.overflowY = "scroll";
     };
+
+    const handleClick = async (e) =>{
+        e.preventDefault();
+        const currentDate = new Date()
+        let dayInd
+        if(filterDate){
+            dayInd = new Date(filterDate).getDay();
+        }else{
+            dayInd = currentDate.getDay();
+        }
+        let day;
+        switch (dayInd) {
+            case 0:
+            day = "Sunday";
+            break;
+            case 1:
+            day = "Monday";
+            break;
+            case 2:
+            day = "Tuesday";
+            break;
+            case 3:
+            day = "Wednesday";
+            break;
+            case 4:
+            day = "Thursday";
+            break;
+            case 5:
+            day = "Friday";
+            break;
+            case 6:
+            day = "Saturday";
+            break;
+            default:
+            day = "";
+            break;
+        }
+        const date = new Date().toLocaleString().split(',')[0].split('/').reverse().join('-');
+        await postTicket({
+            price: price,
+            departure_date_time: filterDate ? filterDate : date,
+            company_id: company_id,
+            day_name: day,
+            route_id: route_id,
+        });
+        window.location.reload();
+    }
 
     return (
         <>
@@ -38,7 +86,7 @@ export const TicketReservation = () => {
                                 <input className="addline-input" type="text" value={user.phone_number} readOnly="readonly" />
                             </div>
                             <div>
-                                <button className="btnsty" onClick={handleModalClose}>
+                                <button className="btnsty" onClick={handleClick}>
                                     Rezerviši
                                 </button>
                             </div>
