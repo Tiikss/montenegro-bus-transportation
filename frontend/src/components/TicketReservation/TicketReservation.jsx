c12dba49f9ddfa69a31d87d3b0e324239b493a1a
Clipz1204 <aleksa.sljivancanin.as@gmail.com>
2024-08-07 14:18:02 +0200

    Add necessary fields for ticket reservation

--- frontend/src/components/TicketReservation/TicketReservation.jsx
+++ frontend/src/components/TicketReservation/TicketReservation.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 import "./ticket-reservation.css";
 
 export const TicketReservation = () => {
+    const { user } = useContext(AuthContext);
+
     const handleModalClose = () => {
         document
             .getElementById("ticket-reservation-modal")
@@ -23,51 +27,23 @@ export const TicketReservation = () => {
                     <div className="ticket-reservation-info-container">
                         <div className="ticket-reservation-info">
                             <div>
-                                <h2>Ime i prezime:</h2>
-                                <input type="text" />
+                                <h2>Ime i prezime:</h2><input className="addline-input" type="text" value={user.full_name} readOnly="readonly" />
                             </div>
                             <div>
                                 <h2>Email:</h2>
-                                <input type="email" />
+                                <input className="addline-input" type="email" value={user.email} readOnly="readonly" />
                             </div>
                             <div>
-                                <h2>Izaberite sjedište</h2>
-                                <input type="text" />
-                            </div>
-                        </div>
-                        <div className="ticket-reservation-info">
-                            <div>
-                                <h2>Način plaćanja:</h2>
-                                <label htmlFor="card">Kartica:</label>
-                                <input
-                                    type="radio"
-                                    name="payment"
-                                    value="card"
-                                />
-                                <label htmlFor="in-person">Uživo:</label>
-                                <input
-                                    type="radio"
-                                    name="payment"
-                                    value="in-person"
-                                />
+                                <h2>Broj telefona:</h2>
+                                <input className="addline-input" type="text" value={user.phone_number} readOnly="readonly" />
                             </div>
                             <div>
-                                <h2>Finalni podaci:</h2>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Libero asperiores sapiente
-                                    excepturi voluptate suscipit ratione nobis
-                                    quas illum adipisci unde iure debitis, quod,
-                                    quam veritatis impedit quaerat. Molestias,
-                                    minima ex?
-                                </p>
-                            </div>
-                            <div>
-                                <button onClick={handleModalClose}>
+                                <button className="btnsty" onClick={handleModalClose}>
                                     Rezerviši
                                 </button>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
